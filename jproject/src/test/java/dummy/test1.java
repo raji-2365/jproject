c5f042b8356754a50bc6a02b02de378c33fed44f
RAJYALAXMI REDDY <RAJYALAXMI REDDY@LAPTOP-IJ0C13NH>
2023-01-09 21:24:09 +0530

    run successfully

---
+++ jproject/src/test/java/dummy/test1.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test1 {
+	@Test
+	public void m1() {
+		Reporter.log("Sruthi",true);
+	}
+
+}
